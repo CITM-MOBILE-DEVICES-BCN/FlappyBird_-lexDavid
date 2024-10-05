ab291b4a0127e02e63c0e24cdc9e79d4ec18f356
DavidLastra8 <david.lastra@estudiantat.upc.edu>
2024-10-05 17:48:18 +0200

    Animacion pajaro

--- FlappyBird/Assets/Scripts/FlyMovement.cs
+++ FlappyBird/Assets/Scripts/FlyMovement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class FlyMovement : MonoBehaviour
 {
-    private float velocity = 1.5f;
-    private float rotationSpeed = 6f;
+    private float velocity = 1.75f;
+    private float rotationSpeed = 10f;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
@@ -18,11 +18,11 @@ public class FlyMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.velocity = transform.up * velocity;
+            rb.velocity = Vector2.up * velocity;
         }
     }
     private void FixedUpdate()
     {
-        transform.rotation = Quaternion.Euler(0, 0, rotationSpeed * rb.velocity.y);
+        transform.rotation = Quaternion.Euler(0, 0, rb.velocity.y * rotationSpeed);
     }
 }
